2d82f0bbde1ece9167d12d880f2b5b5e1ca177d9
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2021-10-09 08:30:45 +0530

    nav made responsive; gallery carousel added

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.header_nav')
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('shown')
+  burger.classList.toggle('cross')
+})
+
+
+
+new Splide('.splide', {
+  type: 'loop',
+  drag: 'free',
+  perPage: 3,
+  focus: 'center',
+  autoplay: true,
+  breakpoints: {
+    1000: {
+      perPage: 2
+    },
+    500: {
+      perPage: 1
+    },
+  },
+}).mount()
+
+const arrow = document.querySelector('.arrow')
+document.addEventListener('scroll', () => {
+  const scroll = document.documentElement.scrollTop || document.body.scrollTop
+  scroll > 200 ? arrow.classList.remove('out') : arrow.classList.add('out')
+})
